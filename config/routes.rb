582fafdccf9d5dc52bbb92e37de6ed03d7a3168b
William Meyer <williamjmiii@gmail.com>
2022-03-23 09:07:32 -0400

    Generated sessions controller and create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  #Users
   post "/users" => "users#create"
+
+  #Session
+  post "/session" => "sessions#create"
 end
